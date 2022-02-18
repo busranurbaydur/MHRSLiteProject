cc0525f983813f3a52cb9e993391b8034037033a
busranurbaydur <busranurbaydur@outlook.com>
2022-02-18 12:23:05 +0300

    Appointment View Added

---
+++ MHRSLiteUI/Controllers/PatientController.cs
@@ -0,0 +1,68 @@
+﻿using MHRSLiteBusiness.Contracts;
+using MHRSLiteBusiness.EmailService;
+using MHRSLiteEntity.IdentityModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHRSLiteUI.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
+        public PatientController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            RoleManager<AppRole> roleManager, IEmailSender emailSender, IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _emailSender = emailSender;
+            _unitOfWork = unitOfWork;
+            _configuration = configuration;
+           
+        }
+
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+        }
+
+        public IActionResult Appointment()
+        {
+            try
+            {
+                ViewBag.Cities = _unitOfWork.CityRepository.GetAll(orderBy:x=>x.OrderBy(a=>a.CityName));
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                return View();
+            }
+        }
+    }
+}
