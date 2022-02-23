c7d35f322fe3b18825db668e9b1d3c156ca0266e
busranurbaydur <busranurbaydur@outlook.com>
2022-02-23 14:56:24 +0300

    Find Appointment Add

---
+++ MHRSLiteUI/Models/AvailableDoctorAppointmentHoursViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MHRSLiteEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHRSLiteUI.Models
+{
+    public class AvailableDoctorAppointmentHoursViewModel
+    {
+        public Doctor Doctor { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string HourBase { get; set; }
+        public List<string> Hours { get; set; } = new List<string>();  //selin yazsın.... 
+
+        
+    }
+}
