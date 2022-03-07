98cc2a7a28ad3e0bc10111aae79f7c73b5545045
busranurbaydur <busranurbaydur@outlook.com>
2022-03-07 12:13:27 +0300

    Hospital Districts Excel import Added

--- MHRSLiteUI/CreateDefaultData/CreateData.cs
+++ MHRSLiteUI/CreateDefaultData/CreateData.cs
@@ -27,7 +27,120 @@ namespace MHRSLiteUI.CreateDefaultData
             CheckRoles(roleManager);
             CreateCities(environment, unitOfWork);
             CreateClinics(environment, unitOfWork);
+#if DEBUG
+            CreateDistricts(environment, unitOfWork);
+            CreateHospitals(environment, unitOfWork);
+#endif
         }
+
+        private static void CreateHospitals(IWebHostEnvironment environment, IUnitOfWork unitOfWork)
+        {
+            try
+            {
+                var hospitalList = unitOfWork.HospitalRepository
+                    .GetAll().ToList();
+
+                //Provide a path for excel file
+                // Excel dosyasının bulunduğu yolu aldık
+                string path = Path.Combine(environment.WebRootPath, "Excels");
+                string fileName = Path.GetFileName("Hospitals.xlsx");
+                string filePath = Path.Combine(path, fileName);
+                using (var excelBook = new XLWorkbook(filePath))
+                {
+                    var rows = excelBook.Worksheet(1).RowsUsed();
+                    foreach (var item in rows)
+                    {
+                        if (item.RowNumber() > 1
+                            && item.RowNumber() <= rows.Count())
+                        {
+                            var cell = item.Cell(1).Value;
+                            var districtId = Convert.ToInt32(item.Cell(2).Value);
+                            var address = item.Cell(3).Value;//hast adresi
+                            var email = item.Cell(4).Value;//hastane e mail
+                            var latitude = item.Cell(5).Value;//hastane enlem
+                            var longitude = item.Cell(6).Value;//boylam
+                            var phoneNumber = item.Cell(7).Value;//hastane tel
+                            var district = unitOfWork.DistrictRepository.GetFirstOrDefault(x => x.Id == districtId);
+                            Hospital hospital = new Hospital()
+                            {
+                                HospitalName = cell.ToString(),
+                                DistrictId=districtId,
+                                CreatedDate = DateTime.Now,
+                                Address=address.ToString(),
+                                Email=email.ToString(),
+                                Latitude=latitude.ToString(),
+                                Longitude=longitude.ToString(),
+                                PhoneNumber=phoneNumber.ToString()
+                            };
+                            if (hospitalList
+                                .Count(x => x.HospitalName.ToLower()
+                                == cell.ToString().ToLower()&&x.DistrictId==districtId) == 0)
+                            {
+                                unitOfWork.HospitalRepository.Add(hospital);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private static void CreateDistricts(IWebHostEnvironment environment, IUnitOfWork unitOfWork)
+        {
+            try
+            {
+                var districtList = unitOfWork.DistrictRepository
+                    .GetAll().ToList();
+
+                //Provide a path for excel file
+                // Excel dosyasının bulunduğu yolu aldık
+                string path = Path.Combine(environment.WebRootPath, "Excels");
+                string fileName = Path.GetFileName("Districts.xlsx");
+                string filePath = Path.Combine(path, fileName);
+                using (var excelBook = new XLWorkbook(filePath))
+                {
+                    var rows = excelBook.Worksheet(1).RowsUsed();
+                    foreach (var item in rows)
+                    {
+                        if (item.RowNumber() > 1
+                            && item.RowNumber() <= rows.Count())
+                        {
+                            var cell = item.Cell(1).Value; //ilçe adı
+                            var cityId = Convert.ToByte(item.Cell(2).Value);//1
+                            var city = unitOfWork.CityRepository
+                              .GetFirstOrDefault(x => x.Id == cityId);
+                            District district = new District()
+                            {
+                                DistrictName = cell.ToString(),
+                                CityId = cityId,
+                                CreatedDate = DateTime.Now
+                            };
+
+                            if (districtList
+                                .Count(x => x.DistrictName.ToLower()
+                                == cell.ToString().ToLower()
+                                && x.CityId == cityId) == 0)
+                            {
+                                unitOfWork.DistrictRepository.Add(district);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
 
         private static void CreateClinics(IWebHostEnvironment environment, IUnitOfWork unitOfWork)
         {
